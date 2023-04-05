7a101e729f5451a69bc2821be10d2d95072f5dc6
DenisBorisov888 <Ltybc-89@mail.ru>
2023-04-05 22:42:55 +0300

    1:49:13

--- canva.py
+++ canva.py
@@ -1,7 +1,19 @@
 import tkinter
 
+oval_id = None
+
 def create_ball() :
-    canvas.create_oval(100, 100, 200, 200)
+    global oval_id
+    if oval_id is None :
+        oval_id = canvas.create_oval(10, 10, 20, 20, fill = "green")
+    else :
+        print("ПРЕДУПРЕЖДЕНИЕ: Овал уже создан! Не надо сюда кликать!")
+
+def delete_ball() :
+    global oval_id
+    print("В этот момент исчезает...")
+    canvas.delete(oval_id)
+    oval_id = None
 
 root = tkinter.Tk("Главное окно")
 root.geometry("640x480")
