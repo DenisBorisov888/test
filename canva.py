e131705db419a92abb09e3845b151fc74ceb1ef4
DenisBorisov888 <Ltybc-89@mail.ru>
2023-04-06 22:17:36 +0300

    Сделан бегающий за мышкой мяч.

--- canva.py
+++ canva.py
@@ -15,15 +15,27 @@ def delete_ball() :
     canvas.delete(oval_id)
     oval_id = None
 
+
+def click_handler(event) :
+    print(event.x, event.y)
+    if oval_id is not None :
+        canvas.coords(oval_id, (event.x-5, event.y-5, event.x+5, event.y+5))
+
+
 root = tkinter.Tk("Главное окно")
 root.geometry("640x480")
 
-canvas = tkinter.Canvas(root)
-canvas.pack()
+buttons_panel = tkinter.Frame(bg = "red", width = "640")
+button_start = tkinter.Button(buttons_panel, text = "Start", command = create_ball)
+button_start.pack (side = tkinter.LEFT)
+button_stop = tkinter.Button(buttons_panel, text = "Stop", command = delete_ball)
+button_stop.pack(side = tkinter.TOP, anchor = "nw")
+
+canvas = tkinter.Canvas(root, bg = "lightgray")
+canvas.pack(anchor = "nw", fill = tkinter.X)
+canvas.bind("<Button>", click_handler)
 
 button_start = tkinter.Button(root, text="Start", command=create_ball)
 button_start.pack()
-
-
 
 root.mainloop()
