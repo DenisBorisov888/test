5c363e780a31b843d30e46c5eaf26863a018c098
DenisBorisov888 <Ltybc-89@mail.ru>
2023-04-16 23:36:46 +0300

    11 урок 01:53:00

--- fly_baii_11.py
+++ fly_baii_11.py
@@ -2,20 +2,24 @@ import tkinter
 import time
 from random import randint
 
+canvas_width = 640
+canvas_height = 480
+
+
+
+#------------------GAME CONTROLLER--------------------
+
 #Режим игры - игра идёт или нет
 game_began = False
 sleep_time = 50 #Милисекунды
 scores = 0
 
 
-
-#------------------GAME CONTROLLER--------------------
 def tick() :
     time_label.after(sleep_time, tick)
-    time_label["text"] = time.strftime("%H:%M:%S")
+    time_label["text"] = time.strftime('%H:%M:%S')
     if game_began:
         game_step()
-
 
 
 def button_game_start_handler () :
@@ -34,7 +38,7 @@ def button_game_stop_handler () :
 
 #-----------GAME MODEL:-----------
 initial_balls_number = 5
-balls = []
+balls = [] #Список объектов типа Ball
 
 
 def game_start() :
@@ -55,29 +59,29 @@ def game_step() :
 class Ball:
     def __init__(self) :
         self.r = randint(10, 30)
-        self.x = randint(0 + self.r, 639 - self.r)
-        self.y = randint(0 + self.r, 479 - self.r)
+        self.x = randint(0 + self.r, canvas_width - self.r)
+        self.y = randint(0 + self.r, canvas_height - self.r)
         self.dx = randint(-4, 4)
         self.dy = randint(-4, 4)
         self.oval_id = canvas.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = "green")
 
-def delete(self) :
-    canvas.delete(self.oval_id)
-    self.oval_id = None
+    def delete(self) :
+        canvas.delete(self.oval_id)
+        self.oval_id = None
 
-def step(self):
-    """
-    Сдвигет шарик в соответствии с его скоростью
-    """
-    if self.oval_id is not None:
-        self.x += self.dx
-        self.y += self.dy
-        if self.x + self.r >= 639 or self.x - self.r <= 0:
-            self.dx = -self.dx
-        if self.y + self.r >= 479 or self.y - self.r <= 0:
-            self.dy = -self.dy
-        canvas.coords(self.oval_id, (self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r))
+    def step(self):
+        """ Сдвигет шарик в соответствии с его скоростью
+        """
+        if self.oval_id is not None:
+            self.x += self.dx
+            self.y += self.dy
+            if self.x + self.r >= canvas_width or self.x - self.r <= 0:
+                self.dx = -self.dx
+            if self.y + self.r >= canvas_height or self.y - self.r <= 0:
+                self.dy = -self.dy
+            canvas.coords(self.oval_id, (self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r))
 
+"""
 def click_handler(event) :
     global  x, y, r, scores_text, scores
     print(event.x, event.y)
@@ -90,27 +94,26 @@ def click_handler(event) :
             x = randint(0 + r, 639 - r)
             y = randint(0 + r, 479 - r)
         canvas.coords(oval_id, (x - r, y - r, x + r, y + r))
-
+"""
 
 #-------------GAME VIEW-------------------
 root = tkinter.Tk("Лопни шарик!")
-root.geometry("640x480")
 
-buttons_panel = tkinter.Frame(bg = "gray", width = "640")
+buttons_panel = tkinter.Frame(bg = "gray", width = canvas_width)
 buttons_panel.pack(side=tkinter.TOP, anchor="nw", fill=tkinter.X)
 button_start = tkinter.Button(buttons_panel, text = "Start", command = button_game_start_handler)
 button_start.pack (side = tkinter.LEFT)
 button_stop = tkinter.Button(buttons_panel, text = "Stop", command = button_game_stop_handler)
 button_stop.pack(side = tkinter.LEFT)
 
-time_label = tkinter.Label(font = "sans 14")
+time_label = tkinter.Label(buttons_panel, font = "sans 14")
 time_label.pack(side = tkinter.LEFT)
 
 
 scores_text = tkinter.Label(buttons_panel, text = "Ваши очки: 0")
 scores_text.pack(side = tkinter.RIGHT)
 
-canvas = tkinter.Canvas(root, bg = "lightgray")
+canvas = tkinter.Canvas(root, bg = "lightgray", width = canvas_width, height = canvas_height)
 canvas.pack(anchor = "nw", fill = tkinter.BOTH, expand = 1)
 canvas.bind("<Button>", click_handler)
 
